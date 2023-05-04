1dc44f6e262cc77694be06ae51b14df0de456f43
Duke_Ferdinand <doug@dougflynn.dev>
2023-05-03 23:56:10 -0700

    Begin work on dashboard layout

--- lib/Layout.tsx
+++ lib/Layout.tsx
@@ -10,18 +10,18 @@ export const Layout = (props: PropsWithChildren) => {
       className={`flex min-h-screen flex-col items-center justify-between p-24 bg-black ${inter.className}`}
     >
       {props.children}
-      <div className={`text-center`}>
-        <p className={`text-white`}>
+      <footer className={`text-center`}>
+        <p className={`text-white tracking-wide`}>
           MIT License - Not affiliated OBS, Twitch, or Amazon.
         </p>
         <a
           target="_blank"
-          className={`text-purple-600`}
+          className={`text-purple-600 tracking-wide`}
           href="https://twitch.tv/duke_ferdinand"
         >
           Coded by Duke_Ferdinand
         </a>
-      </div>
+      </footer>
     </main>
   );
 };
--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,7 +1,26 @@
 import { NextPage } from "next";
 
 const Dashboard: NextPage = () => {
-  return <div>hello</div>;
+  return (
+    <section className={`w-full md:w-5/6`}>
+      <h2 className={`text-white font-black text-2xl tracking-wide`}>
+        Hello, ADD USERNAME
+      </h2>
+      <p className={`text-white tracking-wide`}>Edit your layouts below</p>
+      <article className={`text-white mt-5`}>
+        <div className=" border-dashed border rounded-2xl flex justify-center p-5 mb-4">
+          <button
+            className={`py-3 px-5 bg-blue-900 hover:bg-blue-950 rounded-xl`}
+          >
+            Create a Layout
+          </button>
+        </div>
+        <div className={`bg-gray-900 hover:bg-gray-800 p-5 rounded-xl`}>
+          <h3 className={`text-xl`}>TikTok OBS Layout</h3>
+        </div>
+      </article>
+    </section>
+  );
 };
 
 export default Dashboard;
